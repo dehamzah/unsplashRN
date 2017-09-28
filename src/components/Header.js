6a33e3bad3ddc488271939cc6cd08295d0b31536
Dede Hamzah <dehamzah@gmail.com>
2017-09-28 21:10:40 +0700

    Update style Header
    Menambahkan style pada komponen Header dengan menggunakan inline-style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const Header = () => (
-    <View>
-        <Text>Unsplash</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>Unsplash</Text>
     </View>
 );
+
+const styles = {
+    container: {
+        padding: 15,
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#525252'
+    }
+}
 
 export default Header;
