4bc89f872c5d35897cbf1564723d5c386cd5f326
Dede Hamzah <dehamzah@gmail.com>
2017-09-28 20:44:19 +0700

    Hello World
    Hapus contoh kode dari generator dan kita mulai dari awal dengan
    menampilkan kata sakti `Hello World!`

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+const App = () => <Text>Hello World!</Text>;
+
+AppRegistry.registerComponent('unsplashRN', () => App);
