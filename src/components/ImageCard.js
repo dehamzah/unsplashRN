67e9a13e40f5606454ed7defdf5d8d77c72c73ac
Dede Hamzah <dehamzah@gmail.com>
2017-09-28 22:49:12 +0700

    Menambahkan Author Info pada CardImage

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -8,6 +8,13 @@ const ImageCard = () => (
             style={styles.image}
             resizeMode="cover"
         />
+        <View style={styles.authorContainer}>
+            <Image
+                source={require('../images/user-01.jpeg')}
+                style={styles.authorImage}
+            />
+            <Text style={styles.authorName}>Napaki Muttaqin</Text>
+        </View>
     </View>
 );
 
@@ -19,6 +26,24 @@ const styles = StyleSheet.create({
     image: {
         width: null,
         height: 200
+    },
+    authorContainer: {
+        paddingVertical: 8,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    authorImage: {
+        width: 35,
+        height: 35,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: '#e8e8e8'
+    },
+    authorName: {
+        marginLeft: 8,
+        color: '#4c4c4c',
+        fontWeight: 'bold',
+        fontSize: 14
     }
 });
 
