2b817ffab4a5779d1f3c7040556ebc3bbd32ef6e
Dede Hamzah <dehamzah@gmail.com>
2017-09-29 07:14:35 +0700

    Menambahkan Love Action
    Belajar menggunakan state untuk menyimpan aksi gambar yang tercintai
    dan tidak dicintai

--- src/components/HeartButton.js
+++ src/components/HeartButton.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 
 export default class HeartButton extends Component {
     constructor(props) {
         super(props);
+        
+        this.state = {
+            isLoved: false
+        }
+    }
+
+    onPressButton = () => {
+        //console.log('you loved this');
+        this.setState(prevState => {
+            return { isLoved: !prevState.isLoved }
+        })
     }
 
     render() {
+        const icon = this.state.isLoved ? require('unsplashRN/src/images/heart-fill.png') : require('unsplashRN/src/images/heart-outline-black.png');
         return (
-            <Image 
-                source={require('unsplashRN/src/images/heart-outline-black.png')} 
-                style={{ width: 25, height: 25 }}
-                resizeMode="contain"
-            />
+            <TouchableOpacity onPress={this.onPressButton}>
+                <Image 
+                    source={icon} 
+                    style={{ width: 25, height: 25 }}
+                    resizeMode="contain"
+                />
+            </TouchableOpacity>
         );
     }
 }
